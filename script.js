4ac23e430cda669ca7a17cbc0310216909f42117
Thaina-oliveira <thaina.ogoncalves@outlook.com>
2024-07-02 15:17:21 -0300

    adicionado menu hamburger e novo cv

--- script.js
+++ script.js
@@ -1,54 +1,29 @@
-// contact form
-const form = document.querySelector(".wrapper");
-const btnCloseForm = document.querySelector(".close-modal");
-const openForm = document.querySelector(".contact-us");
-
-const btnOpenForm = function () {
-  form.classList.remove("hidden-form");
-};
-
-const closeForm = function () {
-  form.classList.add("hidden-form");
-};
-
-btnCloseForm.addEventListener("click", closeForm);
-openForm.addEventListener("click", btnOpenForm);
-
-// pressing esc to close the modal;
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && !form.classList.contains("hidden-form")) {
-    closeForm();
-  }
-});
-
-// sandwich button nav bar
-
-function menuShow() {
-  let menuMobile = document.querySelector(".mobile-menu");
-  console.log();
-
-  if (menuMobile.classList.contains("open-menu")) {
-    menuMobile.classList.remove("open-menu");
-  } else {
-    menuMobile.classList.add("open-menu");
-  }
-}
-
 // reveal elements on scroll
 window.addEventListener("scroll", reveal);
 
 function reveal() {
-  let reveals = document.querySelectorAll(".fade-in");
+  const reveals = document.querySelectorAll(".fade-in");
 
-  for (let i = 0; i < reveals.length; i++) {
+  reveals.forEach((reveal) => {
     let windowheight = window.innerHeight;
-    let revealTop = reveals[i].getBoundingClientRect().top;
+    let revealTop = reveal.getBoundingClientRect().top;
     let revealPoint = 150;
 
     if (revealTop < windowheight - revealPoint) {
-      reveals[i].classList.add("active");
+      reveal.classList.add("active-fade-in");
     } else {
-      reveals[i].classList.remove("active");
+      reveal.classList.remove("active-fade-in");
     }
-  }
+  });
+  s;
 }
+
+// mobibe menu
+const hamburger = document.querySelector(".hamburger");
+
+const offScreenMenu = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
